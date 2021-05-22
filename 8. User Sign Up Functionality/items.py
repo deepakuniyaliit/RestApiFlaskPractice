940dbae0f25a0f1051921aa31b2e0b55391b4368
deepakuniyaliit <deepak.uniyal08@gmail.com>
2021-05-22 18:43:50 +0530

    implement get and put method

--- 8. User Sign Up Functionality/items.py
+++ 8. User Sign Up Functionality/items.py
@@ -83,21 +83,56 @@ class Item(Resource):
 
         return {'message':'Item deleted'}
 
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'],item['name']))
+
+        connection.commit()
+        connection.close()
+
+
     def put(self,name):
         '''
-        Request parsing will allow only few arguments of payload while updating the item details.
-        Without using it, name of the item will also be updated in the dictionary, which is not required.
+        insert or update item
         '''
         request_data = Item.parser.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
+
+        item = self.find_by_name(name)
+        print(item)
+        print(name, request_data['price'])
+        updated_item = {'name':name, 'price':request_data['price']}
+        print(updated_item)
         if item is None:
-            item = {'name':name, 'price':request_data['price']}
-            items.append(item)
+            try:
+                self.insert(updated_item)
+            except:
+                return {"message":"An error occurred updating the data"}, 500
         else:
-            item.update(request_data)
-        return item
+            try:
+                self.update(updated_item)
+            except:
+                return {"message":"An error occurred updating the data"}, 500
+        return updated_item
 
 
 class ItemList(Resource):
     def get(self):
+        '''
+        retrieve every item from the database
+        '''
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name':row[0], 'price':row[1]})
+
+        connection.close()
         return {'items':items}
