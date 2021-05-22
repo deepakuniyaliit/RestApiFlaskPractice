f44829598be370224a3ae89cecff0ebdc5395eb2
deepakuniyaliit <deepak.uniyal08@gmail.com>
2021-05-22 22:40:31 +0530

    add model for item

--- 9. Using Flask SQLAlchemy/resources/items.py
+++ 9. Using Flask SQLAlchemy/resources/items.py
@@ -1,7 +1,8 @@
+from models.item import ItemModel
 import sqlite3
-from sqlite3.dbapi2 import connect
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
+from models.item import ItemModel
 
 class Item(Resource):
     parser = reqparse.RequestParser()
@@ -11,62 +12,30 @@ class Item(Resource):
     help="This field can't be left blank"
     )
 
-    @classmethod
-    def find_by_name(cls, name):
-        '''
-        find item stored in database by name and return it if found
-        '''
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
-
-        if row:
-            return {'item':{'name':row[0], 'price':row[1]}}
-
-
     @jwt_required()
     def get(self, name):
         '''
         retrieve item from the database
         '''
-        item = self.find_by_name(name)
+        item = ItemModel.find_by_name(name)
         if item:
-            return item
+            return item.json()
         return {'message':'item not found'}, 404
-
-
-    @classmethod
-    def insert(cls, item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO items VALUES(?, ?)"
-        cursor.execute(query, (item['name'], item['price']))
-
-        connection.commit()
-        connection.close()
-
 
     def post(self,name):
         '''
         store item to the database if it doesn't exists in the databse already
         '''
-        if self.find_by_name(name):
+        if ItemModel.find_by_name(name):
             return {'message':"An item with name '{}' already exits.".format(name)}, 400
 
         request_data = Item.parser.parse_args()
-        item = {"name":name, "price":request_data['price']}
+        item = ItemModel(name, request_data['price'])
         try:
-            self.insert(item)
+            item.insert()
         except:
             return {"message":"An error occurred inserting the data"}, 500
-
-        return item, 201
-
+        return item.json(), 201
 
     def delete(self,name):
         '''
@@ -74,50 +43,32 @@ class Item(Resource):
         '''
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
-
         query = "DELETE FROM items WHERE name=?"
         cursor.execute(query, (name,))
-
         connection.commit()
         connection.close()
-
         return {'message':'Item deleted'}
-
-
-    @classmethod
-    def update(cls, item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "UPDATE items SET price=? WHERE name=?"
-        cursor.execute(query, (item['price'],item['name']))
-
-        connection.commit()
-        connection.close()
-
-
+        
     def put(self,name):
         '''
         insert or update item
         '''
         request_data = Item.parser.parse_args()
 
-        item = self.find_by_name(name)
-        print(item)
-        print(name, request_data['price'])
-        updated_item = {'name':name, 'price':request_data['price']}
-        print(updated_item)
+        item = ItemModel.find_by_name(name)
+        updated_item = ItemModel(name, request_data['price'])
+        print(updated_item.name, updated_item.price)
         if item is None:
             try:
-                self.insert(updated_item)
+                updated_item.insert()
             except:
                 return {"message":"An error occurred updating the data"}, 500
         else:
             try:
-                self.update(updated_item)
+                updated_item.update()
             except:
                 return {"message":"An error occurred updating the data"}, 500
-        return updated_item
+        return updated_item.json()
 
 
 class ItemList(Resource):
