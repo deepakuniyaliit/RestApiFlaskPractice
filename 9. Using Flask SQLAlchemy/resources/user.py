94ef3ab0721886ba96e0fabe8265f2d3d78cf4e8
deepakuniyaliit <deepak.uniyal08@gmail.com>
2021-05-22 19:09:44 +0530

    move user class to new file inside models package

--- 9. Using Flask SQLAlchemy/resources/user.py
+++ 9. Using Flask SQLAlchemy/resources/user.py
@@ -1,5 +1,4 @@
 import sqlite3
-from sqlite3.dbapi2 import connect
 from flask_restful import Resource, reqparse
 
 
@@ -27,44 +26,3 @@ class UserRegister(Resource):
         connection.close()
 
         return {"message": "user created successfully."}, 201
-
-
-class User:
-    def __init__(self, _id, username, password):
-        self.id = _id
-        self.username = username
-        self.password = password
-
-    @classmethod
-    def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,))
-        row=result.fetchone()
-        if row:
-            user = cls(*row)
-            # user = cls(row[0], row[1], row[2])
-        else:
-            user = None
-
-        connection.close()
-        return user
-
-
-    @classmethod
-    def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row=result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
