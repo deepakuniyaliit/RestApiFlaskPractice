7954115daa70e2e05ebf9fce92b558b49bb3d45b
deepakuniyaliit <deepak.uniyal08@gmail.com>
2021-05-23 18:59:02 +0530

    create store resource

--- 9. Using Flask SQLAlchemy/app9.py
+++ 9. Using Flask SQLAlchemy/app9.py
@@ -1,9 +1,12 @@
+from models.store import StoreModel
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.items import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -11,15 +14,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'deepak'
 api = Api(app)
 
-
+# import every resource or model for which we need to create tables automatically
 @app.before_first_request
 def create_tables():
     db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
+api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
